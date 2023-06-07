45e7f17a5e02dd3377440a07f93c1cbf683e9978
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-06-07 06:08:41 -0700

    added api

---
+++ lorenzoblogapi/finance/finance.php
@@ -0,0 +1,54 @@
+<?php
+
+require('../header.php');
+
+$response = array();
+
+//blog_category_id  = finace
+
+if(!isset($_GET['api_key'])) 
+{
+    //erorr
+    $response['error'] = true;
+    $response['message'] = 'provide an api key';
+}else 
+
+{
+    $api_key = $_ENV['API_KEY'];
+
+  if ($_GET['api_key'] !== $api_key) {
+
+      //erorr
+      $response['error'] = true;
+      $response['message'] = 'Invalid api key';
+
+  }else {
+
+    $stmt = $conn->prepare("SELECT * FROM blog WHERE blog_category_id = 2");
+
+    if ($stmt) {
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $row['blog category'] = 'Finance';
+            
+            $blog_post[] = $row;
+
+        }
+
+        $response['error'] = false;
+        $response['blog post'] = $blog_post;
+        $response['message'] = 'blog post returned successfully';
+
+
+        $stmt->close();
+    } else {
+        $response['error'] = true;
+        $response['message'] = 'error';
+    }
+  }
+}
+
+echo json_encode($response);
+?>
